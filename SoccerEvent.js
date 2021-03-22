b103ed567c6b09d3d134508d7ea76e3832c9665f
MaximKokosh <49312437+MaximKokosh@users.noreply.github.com>
2021-03-22 19:38:44 +0300

    Update SoccerEvent.js

--- SoccerEvent.js
+++ SoccerEvent.js
@@ -1,18 +1,17 @@
 import React, { PureComponent } from "react";
 
 export default class SoccerEvent extends PureComponent {
-   render() { 
-    const { content } = this.props.data.content;
-    return ( <div className={"soccer-event"}>
-            <div className={"soccer-event__time"}>
-                {content.time}
-            </div>
-            <div className={"soccer-event__type"}>
-                {content.event_type}
-            </div>
-            <div className={"soccer-event__text"}>
-                {content.text}
-            </div>
+    render() {
+    return (<div>
+                <div>
+                1
+                </div>
+                 <div>
+                2
+                </div>
+                <div>
+                3
+                </div>
             </div> );
   }
 }
