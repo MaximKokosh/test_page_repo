223534027a07f3156efdeca8708ab69ddfb78035
MaximKokosh <49312437+MaximKokosh@users.noreply.github.com>
2021-03-22 17:41:09 +0300

    Update SoccerEvent.js

--- SoccerEvent.js
+++ SoccerEvent.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from "react";
 
-export default class MyCustomTextPostPart extends PureComponent=> {
+export default class SoccerEvent extends PureComponent {
    render() { 
     const { content } = this.props.data.content;
     return (
